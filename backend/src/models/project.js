e100317f4985f47ff343ceee92bc5d2bb9feb257
Sushmitadasari <sushmitadasari66@gmail.com>
2026-01-05 22:16:17 +0530

    Update project.js

--- backend/src/models/project.js
+++ backend/src/models/project.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-
+// Project model representing a project within a tenant
 const Project = sequelize.define('Project', {
   id: {
     type: DataTypes.UUID,
